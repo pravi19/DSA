5e092286ae558d7454bf7906cb5bd1216c15298b
pravi19 <pavitharan.rp@gmail.com>
2023-12-03 15:32:33 +0530

    DSA Subarray zero sum Question.

--- GFG/HashMap/SubarrayWithZeroSum.java
+++ GFG/HashMap/SubarrayWithZeroSum.java
@@ -1,7 +1,33 @@
 package GFG.HashMap;
+import java.util.*;
+
+/*Q)Sub array with zero sum.Return true or false for given array there exist a subarray sum such that it is equal to zero*/
 
 public class SubarrayWithZeroSum {
     public static void main(String[] args){
-        //code will be added
+       int[] arr={6 ,-2, -9, -5, -9, -6, 6, 2, 9, -8};
+        int[] arr1={4 ,2, -3, 1, 6};
+
+        System.out.println(findsum(arr,arr.length));
+    }
+
+    static boolean findsum(int[] arr,int n)
+    {
+       HashSet<Integer>hs=new HashSet<>();
+
+       hs.add(0);
+
+       int sum=0;
+
+       for(int i=0;i<n;i++){
+           sum+=arr[i];
+           if(hs.contains(sum)){
+               return true;
+           }
+
+           hs.add(sum);
+       }
+
+       return false;
     }
 }
